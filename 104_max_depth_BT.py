5e100726f1c9f91ab0cf1f5c397564564bbb897c
dinobobo <kenzocyp@gmail.com>
2019-09-27 17:59:39 -0400

    Create 104_max_depth_BT.py

---
+++ 104_max_depth_BT.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root:
+            dep_l = self.maxDepth(root.left)
+            dep_r = self.maxDepth(root.right)
+            return 1 + max(dep_l, dep_r)
+        else:
+            return 0
