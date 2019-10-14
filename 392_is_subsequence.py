8cd280b902440d75e42b91cd22eed47c9aff195c
Yanping Cai <40920652+dinobobo@users.noreply.github.com>
2019-10-14 12:19:35 -0400

    Update 392_is_subsequence.py

--- 392_is_subsequence.py
+++ 392_is_subsequence.py
@@ -21,5 +21,27 @@ class Solution(object):
 
 #Using binary search. For a incoming stream of s, it is better to use binary search since we don't have to go through t anymore.
 
+from collections import defaultdict
+from bisect import bisect_left
 class Solution(object):
     def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        dic = defaultdict(list)
+        for i, j in enumerate(t):
+            dic[j].append(i)
+            
+        
+        k = 0
+        for i in s:
+            if i not in dic.keys():
+                return False
+            j = bisect_left(dic[i], k)
+            if j == len(dic[i]):
+                return False
+            else:
+                k = dic[i][j] + 1
+        return True
